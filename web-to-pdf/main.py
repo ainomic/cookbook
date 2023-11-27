385f47564fb124755bb005274acef3784a0a654f
Ainomic Dev <contact@ainomic.in>
2023-11-27 16:01:35 +0530

    Add options to PdfConverter

--- web-to-pdf/main.py
+++ web-to-pdf/main.py
@@ -2,6 +2,7 @@ from pdf_converter import PdfConverter
 
 if __name__ == '__main__':
     is_converted = PdfConverter.from_url(
-        "https://generativeai.pub/building-a-streamlit-q-a-app-using-openais-assistant-api-8193f718e7ed",
-        "medium-1.pdf")
+        url="https://generativeai.pub/building-a-streamlit-q-a-app-using-openais-assistant-api-8193f718e7ed",
+        output_path="outputs/medium-2.pdf",
+        options={'orientation': "Landscape"})
     print('Conversion status:', is_converted)
--- web-to-pdf/pdf_converter.py
+++ web-to-pdf/pdf_converter.py
@@ -1,5 +1,34 @@
+from typing import Any, Dict
+
+
 class PdfConverter:
+    _options = {
+        'encoding': "UTF-8",
+        'no-outline': None,
+        # 'orientation': 'Landscape',
+        # 'page-size': 'Letter',
+        # 'margin-top': '0.75in',
+        # 'margin-right': '0.75in',
+        # 'margin-bottom': '0.75in',
+        # 'margin-left': '0.75in',
+        # 'custom-header': [
+        #     ('Accept-Encoding', 'gzip')
+        # ],
+        # 'cookie': [
+        #     ('cookie-empty-value', '""')
+        #     ('cookie-name1', 'cookie-value1'),
+        #     ('cookie-name2', 'cookie-value2'),
+        # ],
+    }
+
     @staticmethod
-    def from_url(url, output_path):
+    def from_url(url, output_path, options: Dict[str, Any] = None):
         from pdfkit import from_url
-        return from_url(url=url, output_path=output_path)
+        final_options = PdfConverter._options.copy()
+        if options:
+            final_options.update(options)
+        return from_url(
+            url=url,
+            output_path=output_path,
+            options=final_options,
+        )
