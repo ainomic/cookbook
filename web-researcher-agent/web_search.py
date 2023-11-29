533caf98a240a6243498ab331a369f69eb44066f
Ainomic Dev <contact@ainomic.in>
2023-11-29 10:24:07 +0530

    Web search updated

--- web-researcher-agent/web_search.py
+++ web-researcher-agent/web_search.py
@@ -1,14 +1,46 @@
 from langchain.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults
 from langchain.utilities import DuckDuckGoSearchAPIWrapper
+import requests
+from bs4 import BeautifulSoup
 
-search = DuckDuckGoSearchRun()
-print(search.run("What is the capital of France?"))
 
-print("-----")
-search = DuckDuckGoSearchResults()
-print(search.run("What is the capital of France?"))
+RESULTS_PER_QUESTION = 3
+ddg_search = DuckDuckGoSearchAPIWrapper()
 
-print("-----")
-wrapper = DuckDuckGoSearchAPIWrapper(region="en-IN", time="d", max_results=2)
-search = DuckDuckGoSearchResults(api_wrapper=wrapper)
-print(search.run("What is the capital of France?"))
+
+def scrape_text(url: str):
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            page_text = soup.get_text(separator=" ", strip=True)
+
+            return page_text
+        else:
+            return f"Failed to retrieve the webpage: status code {response.status_code}"
+    except Exception as e:
+        print(e)
+        return f"Failed to retrieve the webpage: {e}"
+
+
+def web_search(query: str, num_results: int):
+    results = ddg_search.results(query, num_results)
+    return [r["link"] for r in results]
+
+
+def get_links(query: str):
+    return [{
+        "url": url,
+        "question": query,
+    } for url in web_search(query, RESULTS_PER_QUESTION)]
+
+
+if __name__ == "__main__":
+    query = "What is the capital of France?"
+    links = web_search(query, RESULTS_PER_QUESTION)
+    scraped_data = []
+    for link in links:
+        scraped_text = scrape_text(link)
+        scraped_data.append(scraped_text)
